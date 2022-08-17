f01d8a5510d8a98532a76edec3d189829b4e1210
Ryusei Makiguchi <makiguchir@gmail.com>
2022-08-17 16:47:44 -0300

    finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :restaurants, except: %i[edit update destroy] do
+    resources :reviews, only: :create
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Cleaning database"
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+palacio_de_pizza = { name: 'palacio_pizza', address: 'corrientes 1020', phone_number: '2323', category: 'chinese'}
+guerrin = { name: 'guerrin', address: '9 de julio 3030', phone_number: '5323', category: 'japanese'}
+guitarrita = { name: 'guitarrita', address: 'libertador 3030', phone_number: '3232', category: 'chinese'}
+los_inmortales = { name: 'los_inmortales', address: 'las heras 3330', phone_number: '32323', category: 'french'}
+romario = { name: 'romario', address: 'ibera 333', phone_number: '13323', category: 'japanese'}
+
+pizzerias = [palacio_de_pizza, guerrin, guitarrita, los_inmortales, romario]
+
+pizzerias.each do |pizza|
+  restaurant = Restaurant.create!(pizza)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished"
